a594d63d010128bd061cf23394586425be14f6cf
casept <davids.paskevics@gmail.com>
2019-11-07 01:17:06 +0100

    Add nix reproducible build env setup

---
+++ shell.nix
@@ -0,0 +1,24 @@
+
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  rustnightly = (nixpkgs.latest.rustChannels.nightly.rust.override { extensions = [ "rust-src" "rls-preview" "rust-analysis" "rustfmt-preview" ];});
+
+  nixPackages = [
+    nixpkgs.gdb
+	nixpkgs.git
+	nixpkgs.zsh
+	nixpkgs.neovim
+	nixpkgs.vscode
+	nixpkgs.mgba
+	rustnightly
+	nixpkgs.cargo-make
+	nixpkgs.cargo-xbuild
+	nixpkgs.gcc-arm-embedded
+  ];
+in
+with nixpkgs;
+stdenv.mkDerivation {
+  name = "sandbox-gba-workspace";
+  buildInputs = nixPackages;
+}
