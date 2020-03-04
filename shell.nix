638f479a841a35a6db8b9029e0456209c9025338
casept <davids.paskevics@gmail.com>
2020-03-04 21:03:52 +0100

    Reorganize shell.nix and make it work with --pure

--- shell.nix
+++ shell.nix
@@ -5,14 +5,18 @@ let
   rustnightly = (nixpkgs.latest.rustChannels.nightly.rust.override { extensions = [ "rust-src" "rls-preview" "rust-analysis" "rustfmt-preview" ];});
 
   nixPackages = [
-    nixpkgs.gdb
-	nixpkgs.mgba
 	rustnightly
 	nixpkgs.cargo-make
 	nixpkgs.cargo-xbuild
 	nixpkgs.gcc-arm-embedded
+	nixpkgs.cacert
+
 	nixpkgs.mindustry
-	nixpkgs.python3
+
+    nixpkgs.python3
+
+    nixpkgs.gdb
+	nixpkgs.mgba
   ];
 in
 nixpkgs.mkShell {
