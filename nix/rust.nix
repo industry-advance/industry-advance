a27d6ea8a97b6d4f545b654f9409b24bddc74793
casept <davids.paskevics@gmail.com>
2020-03-09 19:33:11 +0100

    Revamp nix

---
+++ nix/rust.nix
@@ -0,0 +1,15 @@
+{ sources ? import ./sources.nix }:
+
+let
+  pkgs =
+    import sources.nixpkgs { overlays = [ (import sources.nixpkgs-mozilla) ]; };
+  channel = "nightly";
+  date = "2020-03-08";
+  targets = [ ];
+  extensions = [ "rust-src" "rls-preview" "rust-analysis" "rustfmt-preview" ];
+  rustChannelOfTargetsAndExtensions = channel: date: targets: extensions:
+    (pkgs.rustChannelOf { inherit channel date; }).rust.override {
+      inherit targets extensions;
+    };
+  chan = rustChannelOfTargetsAndExtensions channel date targets extensions;
+in chan
--- shell.nix
+++ shell.nix
@@ -1,28 +1,30 @@
-
 let
-  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-  rustnightly = (nixpkgs.latest.rustChannels.nightly.rust.override { extensions = [ "rust-src" "rls-preview" "rust-analysis" "rustfmt-preview" ];});
+  sources = import ./nix/sources.nix;
+  rust = import ./nix/rust.nix { inherit sources; };
+  nixpkgs = import sources.nixpkgs { };
 
   nixPackages = [
-	rustnightly
-	nixpkgs.cargo-make
-	nixpkgs.cargo-xbuild
-	nixpkgs.gcc-arm-embedded
-	nixpkgs.cacert
+    rust
 
-	nixpkgs.mindustry
+    # Build tooling
+    nixpkgs.cargo-make
+    nixpkgs.cargo-xbuild
+    nixpkgs.gcc-arm-embedded
+    nixpkgs.cacert
 
+	# Reference implementation
+    nixpkgs.mindustry
+
+	# Execution and maintenance of python glue
     nixpkgs.python37
-	nixpkgs.python37Packages.black
-	nixpkgs.python37Packages.flake8
-	nixpkgs.python37Packages.pydocstyle
-	nixpkgs.python37Packages.mypy
+    nixpkgs.python37Packages.black
+    nixpkgs.python37Packages.flake8
+    nixpkgs.python37Packages.pydocstyle
+    nixpkgs.python37Packages.mypy
 
+	# Debugging and testing
     nixpkgs.gdb
-	nixpkgs.mgba
+    nixpkgs.mgba
+    nixpkgs.xwayland
   ];
-in
-nixpkgs.mkShell {
-  buildInputs = nixPackages;
-}
+in nixpkgs.mkShell { buildInputs = nixPackages; }
