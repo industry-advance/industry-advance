9e43988193073857e28d18ac15761ee72a9ca3da
casept <davids.paskevics@gmail.com>
2020-06-23 23:31:32 +0200

    Fix panics in several systems if no component of expected type exists

--- src/systems/item_movement_system.rs
+++ src/systems/item_movement_system.rs
@@ -6,49 +6,54 @@ use crate::{debug_log, debug_log::Subsystems};
 use alloc::vec::Vec;
 
 use tiny_ecs::Entities;
-
+fn have_sources(ecs: &mut Entities) -> bool {
+    match ecs.borrow_mut::<ItemSourceComponent>() {
+        Ok(_) => return true,
+        Err(_) => return false,
+    }
+}
 pub fn tick(ecs: &mut Entities) {
-    let mut sources = ecs.borrow_mut::<ItemSourceComponent>().unwrap();
-    /* TODO: We want to support sinking items into entities that don't have an inventory,
-     but tiny_ecs unfortunately does not seem to like trait objects
-     (it doesn't realize that it should extract all objects that implement the trait, not
-     the trait itself).
-    */
-    let mut inventories = ecs.borrow_mut::<InventoryComponent>().unwrap();
-    // We're only interested in sources which have targets and are dumping
-    let mut active_sources: Vec<&mut ItemSourceComponent> = sources
-        .iter_mut()
-        .filter(|(_i, source)| source.dump_enabled && source.targets != [None; 4])
-        .map(|(_i, source)| source)
-        .collect();
+    if have_sources(ecs) {
+        let mut sources = ecs.borrow_mut::<ItemSourceComponent>().unwrap();
+        /* TODO: We want to support sinking items into entities that don't have an inventory,
+         but tiny_ecs unfortunately does not seem to like trait objects
+         (it doesn't realize that it should extract all objects that implement the trait, not
+         the trait itself).
+        */
+        let mut inventories = ecs.borrow_mut::<InventoryComponent>().unwrap();
+        // We're only interested in sources which have targets and are dumping
+        let mut active_sources: Vec<&mut ItemSourceComponent> = sources
+            .iter_mut()
+            .filter(|(_i, source)| source.dump_enabled && source.targets != [None; 4])
+            .map(|(_i, source)| source)
+            .collect();
 
-    for source in active_sources.iter_mut() {
-        // TODO: Let source component decide which of the targets gets the item.
-        // TODO: For now, it's always the 1st one that's not None.
-        let target: usize = source
-            .targets
-            .iter()
-            .filter(|&target| *target != None)
-            .map(|&target| target.unwrap())
-            .next()
-            .unwrap();
-        // Check whether the target can accept (meaning it has an InventoryComponent and accepts the item type)
-        match inventories.get_mut(target) {
-            Some(inventory) => {
-                if inventory.check_item_accept(source.dump_item, 1) {
-                    inventory
-                        .insert(source.dump_item, 1)
-                        .unwrap();//.expect("Couldn't insert item into inventory!");
-                    source.did_transfer = true;
-                } else {
-                    debug_log!(
-                        Subsystems::InventorySystem,
-                        "Inventory doesn't accept item, not inserting"
-                    );
-                    source.did_transfer = false;
+        for source in active_sources.iter_mut() {
+            // TODO: Let source component decide which of the targets gets the item.
+            // TODO: For now, it's always the 1st one that's not None.
+            let target: usize = source
+                .targets
+                .iter()
+                .filter(|&target| *target != None)
+                .map(|&target| target.unwrap())
+                .next()
+                .unwrap();
+            // Check whether the target can accept (meaning it has an InventoryComponent and accepts the item type)
+            match inventories.get_mut(target) {
+                Some(inventory) => {
+                    if inventory.check_item_accept(source.dump_item, 1) {
+                        inventory.insert(source.dump_item, 1).unwrap(); //.expect("Couldn't insert item into inventory!");
+                        source.did_transfer = true;
+                    } else {
+                        debug_log!(
+                            Subsystems::InventorySystem,
+                            "Inventory doesn't accept item, not inserting"
+                        );
+                        source.did_transfer = false;
+                    }
                 }
-            }
-            None => panic!("Attempt to transfer items to target without InventoryComponent"),
-        };
+                None => panic!("Attempt to transfer items to target without InventoryComponent"),
+            };
+        }
     }
 }
--- src/systems/mining_system.rs
+++ src/systems/mining_system.rs
@@ -11,38 +11,46 @@ use tiny_ecs::Entities;
 const MAX_BACKUP: MiningProgress =
     MiningProgress::from_bits(0b0000_0000_0000_0000_0000_0001_0000_0000);
 
+fn have_miners(ecs: &mut Entities) -> bool {
+    match ecs.borrow_mut::<MinerComponent>() {
+        Ok(_) => return true,
+        Err(_) => return false,
+    }
+}
 pub fn tick(ecs: &mut Entities, live_entities: &[usize]) {
-    let mut miners = ecs.borrow_mut::<MinerComponent>().unwrap();
-    let mut item_sources = ecs.borrow_mut::<ItemSourceComponent>().unwrap();
-    for id in live_entities {
-        if ecs.entity_contains::<MinerComponent>(*id) {
-            let mut e_miner = miners.get_mut(*id).unwrap();
-            // Each miner has to have an ItemSourceComponent which regulates item transfer.
-            let mut e_item_source = item_sources.get_mut(*id).unwrap();
+    if have_miners(ecs) {
+        let mut miners = ecs.borrow_mut::<MinerComponent>().unwrap();
+        let mut item_sources = ecs.borrow_mut::<ItemSourceComponent>().unwrap();
+        for id in live_entities {
+            if ecs.entity_contains::<MinerComponent>(*id) {
+                let mut e_miner = miners.get_mut(*id).unwrap();
+                // Each miner has to have an ItemSourceComponent which regulates item transfer.
+                let mut e_item_source = item_sources.get_mut(*id).unwrap();
 
-            // Try to output an item if possible
-            if e_miner.item_progress >= ONE_PROGRESS {
-                e_item_source.dump_enabled = true;
-            } else {
-                // If not, stop dumping
-                e_item_source.dump_enabled = false;
-            }
-            if e_miner.backed_up {
-                // Check whether an item was transferred out last tick,
-                // meaning we've got to remove it from the backlog
-                if e_item_source.did_transfer {
-                    e_miner.item_progress -= ONE_PROGRESS;
+                // Try to output an item if possible
+                if e_miner.item_progress >= ONE_PROGRESS {
+                    e_item_source.dump_enabled = true;
+                } else {
+                    // If not, stop dumping
+                    e_item_source.dump_enabled = false;
                 }
-                // We may not be backed up anymore
-                if e_miner.item_progress < MAX_BACKUP {
-                    e_miner.backed_up = false;
-                }
-            } else {
-                // Produce
-                e_miner.item_progress += e_miner.speed;
-                // Not backed up, check whether we should be
-                if e_miner.item_progress >= MAX_BACKUP {
-                    e_miner.backed_up = true;
+                if e_miner.backed_up {
+                    // Check whether an item was transferred out last tick,
+                    // meaning we've got to remove it from the backlog
+                    if e_item_source.did_transfer {
+                        e_miner.item_progress -= ONE_PROGRESS;
+                    }
+                    // We may not be backed up anymore
+                    if e_miner.item_progress < MAX_BACKUP {
+                        e_miner.backed_up = false;
+                    }
+                } else {
+                    // Produce
+                    e_miner.item_progress += e_miner.speed;
+                    // Not backed up, check whether we should be
+                    if e_miner.item_progress >= MAX_BACKUP {
+                        e_miner.backed_up = true;
+                    }
                 }
             }
         }
